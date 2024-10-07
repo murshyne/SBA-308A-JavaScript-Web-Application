fcbb22fd18ee7aed3de1da51f8f90481818d292c
Murshyne <murshyne@gmail.com>
2024-10-07 01:05:24 -0400

    Committed at: Mon, Oct  7, 2024  1:05:23 AM

--- api_module.mjs
+++ api_module.mjs
@@ -1,5 +1,5 @@
-// api.js
 const API_URL = "https://exercisedb.p.rapidapi.com/exercises/bodyPart/back";
+const POST_URL = "https://exercisedb.p.rapidapi.com/exercises";
 
 export const fetchExercises = async (query) => {
   try {
@@ -7,17 +7,36 @@ export const fetchExercises = async (query) => {
       method: "GET",
       headers: {
         "x-rapidapi-host": "exercisedb.p.rapidapi.com",
-        "x-rapidapi-key: 99665d3892mshdcafa0a82446451p12dd3ajsnb1dbafe836f9":
-
-        "Content-Type", "application/json":
+        "x-rapidapi-key": "99665d3892mshdcafa0a82446451p12dd3ajsnb1dbafe836f9", 
+        "Content-Type": "application/json",
       },
     });
 
     if (!response.ok) throw new Error("Network response was not ok");
     const data = await response.json();
-    return data; // Return the entire data structure
+    return data;
   } catch (error) {
     console.error("Failed to fetch exercises:", error);
     return [];
+  }
+};
+
+export const addExercise = async (exercise) => {
+  try {
+    const response = await fetch(POST_URL, {
+      method: "POST",
+      headers: {
+        "x-rapidapi-host": "exercisedb.p.rapidapi.com",
+        "x-rapidapi-key": "99665d3892mshdcafa0a82446451p12dd3ajsnb1dbafe836f9", 
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(exercise),
+    });
+
+    if (!response.ok) throw new Error("Failed to add exercise");
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error adding exercise:", error);
   }
 };
